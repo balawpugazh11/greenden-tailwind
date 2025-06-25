9d905e6cc98d2ef82644ed2a1e6fef8c57a2248b
balawpugazh11 <balawpugazh@gmail.com>
2025-06-25 10:50:11 +0530

    updated files

---
+++ forEach.js
@@ -0,0 +1,8 @@
+const fruits = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango'];
+
+fruits.forEach((ele, index) => {
+    if(ele == "Apple"){
+        console.log( ele+" is the position @ " +index)
+    }
+})
+
